58e0f2555d665d91ecaa0d52d1880e0570bf6b40
Joseph P <jpark041@gmail.com>
2019-07-04 01:39:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,11 +27,7 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  if hash
-    hash[key] = "correctKey"
-  else 
-    hash = {key: nil}
-  end
+ hash[key]
 end
 
 def update_counting_hash(hash, key)
